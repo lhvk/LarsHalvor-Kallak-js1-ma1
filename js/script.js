6a90a3d41c5bfaa496e46277e6dad2f6e72eb50a
lhvk <77023253+lhvk@users.noreply.github.com>
2021-11-04 22:57:31 +0100

    completed MA1

--- js/script.js
+++ js/script.js
@@ -34,7 +34,10 @@ heading.style.fontSize = "2em";
 
 // Question 4
 
+heading.className = "subheading";
+
 // Question 5
+
 const paragraphs = document.querySelectorAll("p");
 
 for (let i = 0; i < paragraphs.length; i++) {
@@ -58,3 +61,33 @@ function listOfCats(list) {
 listOfCats(cats);
 
 // Question 8
+
+function createCats(cats) {
+  let catsHtml = "";
+
+  for (let i = 0; i < cats.length; i++) {
+    let name = cats[i].name;
+    let age = cats[i].age;
+
+    let ageUnknown = "Age unknown";
+
+    if (age) {
+      ageUnknown = age;
+    }
+
+    catsHtml += `
+    <div>
+    <h5>${name}</h5>
+    <p>${ageUnknown}</p>
+    </div>
+    `;
+  }
+
+  return catsHtml;
+}
+
+const catsListHtml = createCats(cats);
+
+const container = document.querySelector(".cat-container");
+
+container.innerHTML = catsListHtml;
